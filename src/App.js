d6f98271b4299f52ca40a26c6d95b6e0f5389065
KanekarSnehal <snehalkanekar43@gmail.com>
2022-03-30 12:15:24 +0530

    add item to list

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
 import "./App.css";
 import { ExpenseForm } from "./components/ExpenseForm/ExpenseForm";
-import { Expenses } from "./components/Expenses/Expenses";
+import { useState } from "react";
+import { ExpenseItem } from "./components/ExpenseItem/ExpenseItem";
+
+const DUMMY_DATA = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 324.4,
+    date: new Date(2022, 3, 23),
+  },
+  {
+    id: "e2",
+    title: "Icecream",
+    amount: 4.47,
+    date: new Date(2022, 3, 23),
+  },
+  {
+    id: "e3",
+    title: "Chocolate",
+    amount: 32.7,
+    date: new Date(2022, 3, 23),
+  },
+  {
+    id: "e4",
+    title: "Coffee",
+    amount: 32.4,
+    date: new Date(2022, 3, 23),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 324.4,
-      date: new Date(2022, 3, 23),
-    },
-    {
-      id: "e2",
-      title: "Icecream",
-      amount: 4.47,
-      date: new Date(2022, 3, 23),
-    },
-    {
-      id: "e3",
-      title: "Chocolate",
-      amount: 32.7,
-      date: new Date(2022, 3, 23),
-    },
-    {
-      id: "e4",
-      title: "Coffee",
-      amount: 32.4,
-      date: new Date(2022, 3, 23),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expensedata = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expensedata);
     expenses.concat(expensedata);
-    console.log(expenses);
-
     addExpenseHandler(expensedata);
   };
 
-  const addExpenseHandler = (expensedata) => {
-    expenses.concat(expensedata);
-    console.log(expenses);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
-
   return (
     <div className="App">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-      <Expenses expenses={expenses}></Expenses>
+      <ExpenseItem expenses={expenses} setExpenses={setExpenses}></ExpenseItem>
     </div>
   );
 };
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,9 +1,10 @@
 import { ExpenseDate } from "../ExpenseDate/ExpenseDate";
 import { useState } from "react";
-const ExpenseItem = ({ expenses }) => {
-  const [amount, setAmount] = useState(expenses.amount);
+export const ExpenseItem = ({ expenses, setExpenses }) => {
   const deleteHandler = (expense) => {
-    setAmount("$100");
+    setExpenses((prevExpense) =>
+      prevExpense.filter((expenseItem) => expenseItem.id !== expense.id)
+    );
     // by using className
     // const ele = document.getElementsByClassName(expense.id);
     // ele[0].remove();
@@ -19,7 +20,7 @@ const ExpenseItem = ({ expenses }) => {
           <li key={expense.id} className={expense.id}>
             <h2>Expense Item</h2>
             <p>Title: {expense.title}</p>
-            <p>Amount: {amount}</p>
+            <p>Amount: {expense.amount}</p>
             <ExpenseDate date={expense.date} />
             <button onClick={() => deleteHandler(expense)}>
               Delete Expense
@@ -31,4 +32,3 @@ const ExpenseItem = ({ expenses }) => {
     </div>
   );
 };
-export default ExpenseItem;
